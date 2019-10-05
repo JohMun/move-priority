d049584be14072bd89033bdfc1d2f6190cb9a8fc
JohMun <munari@hydra-newmedia.com>
2019-10-05 15:51:24 +0200

    basic implenentation of detection

--- main.js
+++ main.js
@@ -1,43 +1,48 @@
-let dragging = false;
+import MovePriority from './move-priority';
 
-document.querySelector('.outer-box').addEventListener('mousedown', () => {
-  console.log('mousedown outer-box');
-  dragging = true;
-});
+new MovePriority(document.querySelector('.outer-box'));
+window.dragula([document.querySelector('#dragula')]);
 
-document.querySelector('.outer-box').addEventListener('mouseup', () => {
-  console.log('mouseup outer-box');
-  dragging = false;
-});
+// let dragging = false;
 
-document.querySelector('.outer-box').addEventListener('mousemove', () => {
-  if (!dragging) return;
-  console.log('move outer-box');
-});
+// document.querySelector('.outer-box').addEventListener('mousedown', () => {
+//   console.log('mousedown outer-box');
+//   dragging = true;
+// });
 
-document.querySelector('.outer-box').addEventListener('touchstart', () => {
-  console.log('pointerdown outer-box');
-  dragging = true;
-});
+// document.querySelector('.outer-box').addEventListener('mouseup', () => {
+//   console.log('mouseup outer-box');
+//   dragging = false;
+// });
 
-document.querySelector('.outer-box').addEventListener('touchend', () => {
-  console.log('pointerup outer-box');
-  dragging = false;
-});
+// document.querySelector('.outer-box').addEventListener('mousemove', () => {
+//   if (!dragging) return;
+//   console.log('move outer-box');
+// });
 
-document.querySelector('.outer-box').addEventListener('touchmove', () => {
-  if (!dragging) return;
-  console.log('move outer-box');
-});
+// document.querySelector('.outer-box').addEventListener('touchstart', () => {
+//   console.log('pointerdown outer-box');
+//   dragging = true;
+// });
 
-document.querySelector('.scroll-box').addEventListener('scroll', () => {
-  console.log('scroll');
-});
+// document.querySelector('.outer-box').addEventListener('touchend', () => {
+//   console.log('pointerup outer-box');
+//   dragging = false;
+// });
 
-document.querySelector('.outer-box').addEventListener('scroll', () => {
-  console.log('scroll window');
-}, true);
+// document.querySelector('.outer-box').addEventListener('touchmove', () => {
+//   if (!dragging) return;
+//   console.log('move outer-box');
+// });
 
-document.querySelector('.scroll-box').addEventListener('touchmove', () => {
-  // e.stopPropagation();
-});
+// document.querySelector('.scroll-box').addEventListener('scroll', () => {
+//   console.log('scroll');
+// });
+
+// document.querySelector('.outer-box').addEventListener('scroll', () => {
+//   console.log('scroll window');
+// }, true);
+
+// document.querySelector('.scroll-box').addEventListener('touchmove', () => {
+//   // e.stopPropagation();
+// });
--- move-priority.js
+++ move-priority.js
@@ -1,20 +1,118 @@
+const defaultOptions = {};
+
 export default class MovePriority {
-  // constructor(el, options) {
-  //   let moving = false;
-  //   let moveStartEvent = null;
+  constructor(el, options = {}) {
+    this.moving = false;
+    this.moveStartEvent = null;
+    this.el = el;
+    this.options = { ...defaultOptions, ...options };
+    this.startEvent = null;
+    this.moveEventsCount = 0;
+    this.mObserver = null;
+    this.detectedMove = false;
+    this.detectMutationTimeout = null;
+    this.canMoveTimeout = null;
+    this.canMove = false;
+    this.detectScrollTimeout = null;
+    this.detectedScroll = false;
 
-  //   const isTouch = Boolean('ontouchstart' in window
-  //     || window.DocumentTouch
-  //     && window.DocumentTouch
-  //     && document instanceof window.DocumentTouch);
+    this.isTouch = Boolean('ontouchstart' in window
+      || window.DocumentTouch
+      && window.DocumentTouch
+      && document instanceof window.DocumentTouch);
 
-  // }
+    this.attachEvents();
+    this.initObserver();
+  }
+
+  initObserver() {
+    // observe other drags
+    this.mObserver = new MutationObserver(mutations => {
+      mutations.forEach(() => {
+        // ignore style changes on the sections -> we change them ourselve during animations
+        // if (Array.from(this.el).some(el => event.target === el)) return;
+        // TODO: the above line is important ... get this working
+        this.detectedMove = true;
+      });
+    });
+    this.mObserver.observe(this.el, {
+      attributes: true,
+      childList: true,
+      subtree: true,
+    });
+  }
 
   attachEvents() {
+    this.el.addEventListener(
+      this.isTouch ? 'touchstart' : 'mousedown',
+      this.startMove.bind(this),
+      false,
+    );
 
+    this.el.addEventListener('scroll', () => this.detectedScroll = true, true);
+
+    window.addEventListener(
+      this.isTouch ? 'touchmove' : 'mousemove',
+      this.move.bind(this),
+      { passive: false },
+    );
+
+    window.addEventListener(
+      this.isTouch ? 'touchend' : 'mouseup',
+      this.stopMove.bind(this),
+      false,
+    );
   }
 
   removeEvents() {
 
+  }
+
+  normalizeEvent(event) {
+    // eslint-disable-next-line prefer-const
+    let { clientX, clientY, timeStamp, target } = event;
+    if (event.targetTouches) {
+      clientX = event.targetTouches && event.targetTouches[0].clientX;
+      clientY = event.targetTouches && event.targetTouches[0].clientY;
+    }
+    return { nativeEvent: event, x: clientX, y: clientY, timeStamp, target };
+  }
+
+  startMove(event) {
+    this.startEvent = this.normalizeEvent(event);
+  }
+
+  move() {
+    if (this.canMove) {
+      if (this.canMoveTimeout) clearTimeout(this.canMoveTimeout);
+      console.log('moving');
+      return;
+    }
+    this.detectMovementPossibility();
+  }
+
+  stopMove() {
+    this.resetValues();
+  }
+
+  resetValues() {
+    this.startEvent = null;
+    this.canMove = false;
+    this.moveEventsCount = 0;
+    this.detectedMove = false;
+    this.detectedScroll = false;
+  }
+
+  detectMovementPossibility() {
+    if (!this.startEvent || this.canMove || this.detectedScroll || this.detectedMove) return;
+    this.moveEventsCount++;
+    const inTime = (Date.now() - this.startEvent.timeStamp) > 100;
+    let calledInRange = this.moveEventsCount > 2 && this.moveEventsCount < 18;
+    if (!this.touch) calledInRange = this.moveEventsCount > 2 && this.moveEventsCount < 100;
+    if (inTime && calledInRange) this.canMove = true;
+    else {
+      clearTimeout(this.canMoveTimeout);
+      this.canMoveTimeout = setTimeout(() => this.resetValues(), 60);
+    }
   }
 }
